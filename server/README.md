83fa36d03a523e1a5ada2a031520b43bb3284ae2
Michal Slovik <mishcos@gmail.com>
2019-04-13 12:56:12 +0200

    Add move_server to diploma_thesis

---
+++ server/README.md
@@ -0,0 +1,11 @@
+# Server part
+
+- php login server
+- restore password and resend to email
+- register new user with email address and password
+- connection to localhost db
+- uploading and saving files
+
+
+Mobile detect using:
+A Pen created at CodePen.io. You can find this one at http://codepen.io/jtangelder/pen/lgELw.
---
+++ server/restorePass.php
@@ -0,0 +1,200 @@
+<?php
+ ob_start();
+ session_start();
+ if( isset($_SESSION['user'])!="" ){
+  header("Location: home.php");
+ }
+ include_once 'dbconnect.php';
+
+ $error = false;
+
+ if ( isset($_POST['btn-signup']) ) {
+
+  // clean user inputs to prevent sql injections
+  // $name = trim($_POST['name']);
+  // $name = strip_tags($name);
+  // $name = htmlspecialchars($name);
+
+  $email = trim($_POST['email']);
+  $email = strip_tags($email);
+  $email = htmlspecialchars($email);
+  //
+  // $pass = trim($_POST['pass']);
+  // $pass = strip_tags($pass);
+  // $pass = htmlspecialchars($pass);
+
+  // basic name validation
+  // if (empty($name)) {
+  //  $error = true;
+  // //  $nameError = "Please enter your full name.";
+  // $nameError = "Prosím, zadajte svoje celé meno.";
+  //
+  // } else if (strlen($name) < 3) {
+  //  $error = true;
+  // //  $nameError = "Name must have atleat 3 characters.";
+  //  $nameError = "Meno musí obsahovať aspoň 3 znaky.";
+  //
+  // } else if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
+  //  $error = true;
+  // //  $nameError = "Name must contain alphabets and space.";
+  // $nameError = "Meno musí obsahovať iba písmena a medzery.";
+  //
+  // }
+
+  //basic email validation
+  if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+   $error = true;
+  //  $emailError = "Please enter valid email address.";
+  $emailError = "Prosím, zadajte správnu emailovú adresu.";
+
+  } else {
+   // check email exist or not
+   $query = "SELECT userEmail FROM users WHERE userEmail='$email'";
+   $result = mysql_query($query);
+   $count = mysql_num_rows($result);
+
+
+   if($count!=0){
+    // $error = true;
+    $emailError = "Zadaný email sa už používa";
+
+    // sending email
+    if(isset($_POST) & !empty($_POST)){
+    	$sql = "SELECT * FROM users WHERE userEmail='$email'";
+    	$res = mysql_query($sql);
+
+    		  echo "Send email to user with password";
+          $password = $res['password'];
+          $to = $res['email'];
+          $subject = "Your Recovered Password";
+
+          $message = "Please use this password to login " . $password;
+          $headers = "From : vivek@codingcyber.com";
+          if(mail($to, $subject, $message, $headers)){
+          	echo "Your Password has been sent to your email id";
+          }else{
+          	echo "Failed to Recover your password, try again";
+          }
+
+    }
+  }
+  }
+  // password validation
+  // if (empty($pass)){
+  //  $error = true;
+  //  $passError = "Prosím zadajte heslo";
+  // } else if(strlen($pass) < 6) {
+  //  $error = true;
+  //  $passError = "Heslo musí obsahovať aspoň 6 znakov.";
+  // }
+
+  // password encrypt using SHA256();
+  $password = hash('sha256', $pass);
+
+  // if there's no error, continue to signup
+  // if( !$error ) {
+  //
+  //  $query = "INSERT INTO users(userName,userEmail,userPass) VALUES('$name','$email','$password')";
+  //  $res = mysql_query($query);
+  //
+  //  if ($res) {
+  //   $errTyp = "success";
+  //   $errMSG = "Úspešne prihlásenie, môžte sa prihlásiť";
+  //   unset($name);
+  //   unset($email);
+  //   unset($pass);
+  //  } else {
+  //   $errTyp = "danger";
+  //   $errMSG = "Neočakávaná chyba pri registrácii, skúste znova.";
+  //  }
+  // }
+ }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Biometrická autentifikácia - obnovenie hesla</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+<div class="container">
+
+ <div id="login-form">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+
+     <div class="col-md-12">
+
+         <div class="form-group">
+             <h2 class="">Obnovenie hesla</h2>
+            </div>
+
+         <div class="form-group">
+             <hr />
+            </div>
+
+            <?php
+   if ( isset($errMSG) ) {
+
+    ?>
+    <div class="form-group">
+             <div class="alert alert-<?php echo ($errTyp=="success") ? "success" : $errTyp; ?>">
+    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+             </div>
+                <?php
+   }
+   ?>
+
+            <!-- <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+             <input type="text" name="name" class="form-control" placeholder="Napíšte svoje meno" maxlength="50" value="<?php echo $name ?>" />
+                </div>
+                <span class="text-danger"><?php echo $nameError; ?></span>
+            </div> -->
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+             <input type="email" name="email" class="form-control" placeholder="Napíšte svoj email" maxlength="40" value="<?php echo $email ?>" />
+                </div>
+                <span class="text-danger"><?php echo $emailError; ?></span>
+            </div>
+
+            <!-- <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+             <input type="password" name="pass" class="form-control" placeholder="Napíšte heslo" maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $passError; ?></span>
+            </div> -->
+
+            <div class="form-group">
+             <hr />
+            </div>
+
+            <div class="form-group">
+             <button type="submit" class="btn btn-block btn-primary" name="btn-signup">Obnovenie hesla</button>
+            </div>
+
+            <div class="form-group">
+             <hr />
+            </div>
+
+            <div class="form-group">
+             <a href="index.php">Prihlásenie</a>
+            </div>
+
+        </div>
+
+    </form>
+    </div>
+
+</div>
+
+</body>
+</html>
+<?php ob_end_flush(); ?>
